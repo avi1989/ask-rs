450f708be2583566befd488ab3ae66027a5c37aa
Avinash Krishna Kumar <me@avinash.is>
2025-09-28 18:01:28 +0530

    Implemented read file tool

--- src/llms.rs
+++ src/llms.rs
@@ -1,32 +1,18 @@
-use std::collections::HashMap;
 use std::env;
 use anyhow;
 use serde::{Deserialize, Serialize};
 use openai_api_rs::v1::{api::OpenAIClient, chat_completion::{self, ChatCompletionRequest}};
-use openai_api_rs::v1::chat_completion::ChatCompletionMessage;
-use openai_api_rs::v1::types;
-use crate::tools::list_all_files;
+use openai_api_rs::v1::chat_completion::{ChatCompletionMessage, ToolCall};
+use crate::tools::{list_all_files_tool, list_all_files, ListFilesRequest, read_file_tool, ListFilesToolRequest, read_file};
 
 fn get_openai_client() -> OpenAIClient {
     let api_key = env::var("OPENAI_API_KEY").unwrap().to_string();
     OpenAIClient::builder().with_api_key(api_key).build().unwrap()
 }
 
-#[derive(Deserialize, Serialize)]
-struct ListFilesRequest {
-    base_path: String,
-}
-
 pub async fn ask_question(question: &str) -> Result<String, Box<anyhow::Error>> {
     let mut client = get_openai_client();
     let model = "gpt-4.1".to_string();
-    let mut tool_props = HashMap::new();
-    tool_props.insert("base_path".to_string(),
-                      Box::new(types::JSONSchemaDefine {
-                          schema_type: Some(types::JSONSchemaType::String),
-                          description: Some("The path to get files from".to_string()),
-                          ..Default::default()
-                      }));
     let mut req = ChatCompletionRequest::new(
         model,
         vec![chat_completion::ChatCompletionMessage {
@@ -36,18 +22,7 @@ pub async fn ask_question(question: &str) -> Result<String, Box<anyhow::Error>> 
             tool_call_id: None,
             tool_calls: None
         }],
-    ).tools(vec![chat_completion::Tool {
-        r#type: chat_completion::ToolType::Function,
-        function: types::Function {
-            name: String::from("list_all_files"),
-            description: Some(String::from("List all files in a directory")),
-            parameters: types::FunctionParameters {
-                schema_type: types::JSONSchemaType::Object,
-                properties: Some(tool_props),
-                required: Some(vec!["base_path".to_string()])
-            }
-        }
-    }]).tool_choice(chat_completion::ToolChoiceType::Auto);
+    ).tools(vec![list_all_files_tool(), read_file_tool()]).tool_choice(chat_completion::ToolChoiceType::Auto);
 
     for _i in 0..10 {
         let response_result = client.chat_completion(req.clone()).await;
@@ -73,19 +48,7 @@ pub async fn ask_question(question: &str) -> Result<String, Box<anyhow::Error>> 
                     tool_call_id: None,
                 });
                 for tool_call in tool_calls {
-                    let name = tool_call.function.name.clone().unwrap();
-                    let arguments = tool_call.function.arguments.unwrap();
-                    let id = tool_call.id;
-                    let mut result: String = String::new();
-                    if name == "list_all_files" {
-                        let args: ListFilesRequest = serde_json::from_str(&arguments).unwrap();
-                        let files = list_all_files(args.base_path.as_str());
-                        for file in files {
-                            result.push_str(&file);
-                            result.push('\n');
-                        }
-                    }
-
+                    let (id, result) = execute_tool_call(tool_call);
                     req.messages.push(
                         ChatCompletionMessage {
                             tool_call_id: Some(id),
@@ -115,15 +78,24 @@ pub async fn ask_question(question: &str) -> Result<String, Box<anyhow::Error>> 
     Err(Box::from(anyhow::anyhow!("No response after 10 attempts")))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_ask_question() {
-        let question = "What is the capital of France?";
-        let answer = ask_question(question).await.unwrap();
-        println!("Answer: {}", answer);
-        assert!(answer.to_lowercase().contains("paris"));
+fn execute_tool_call(tool_call: ToolCall) -> (String, String) {
+    let name = tool_call.function.name.clone().unwrap();
+    let arguments = tool_call.function.arguments.unwrap();
+    let id = tool_call.id;
+    let mut result: String = String::new();
+    if name == "list_all_files" {
+        let args: ListFilesRequest = serde_json::from_str(&arguments).unwrap();
+        let files = list_all_files(args.base_path.as_str());
+        for file in files {
+            result.push_str(&file);
+            result.push('\n');
+        }
     }
+    else if name == "read_file" {
+        let args:ListFilesToolRequest = serde_json::from_str(&arguments).unwrap();
+        result = read_file(args.file_path.as_str());
+        result.push('\n');
+    }
+
+    (id, result)
 }
--- src/tools.rs
+++ src/tools.rs
@@ -1,9 +1,78 @@
+use openai_api_rs::v1::chat_completion::Tool;
+use openai_api_rs::v1::{chat_completion, types};
+use std::collections::HashMap;
 use std::fs;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct ListFilesRequest {
+    pub base_path: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ListFilesToolRequest {
+    pub file_path: String,
+}
 
 pub fn list_all_files(base_path: &str) -> Vec<String> {
-    fs::read_dir(base_path).unwrap()
+    fs::read_dir(base_path)
+        .unwrap()
         .map(|e| e.unwrap().file_name().to_string_lossy().to_string())
         .collect()
+}
+
+pub fn read_file(file_path: &str) -> String {
+    fs::read_to_string(file_path).unwrap()
+}
+
+pub fn read_file_tool() -> Tool {
+    let mut tool_props = HashMap::new();
+    tool_props.insert(
+        "file_path".to_string(),
+        Box::new(types::JSONSchemaDefine {
+            schema_type: Some(types::JSONSchemaType::String),
+            description: Some("The path to the file to read".to_string()),
+            ..Default::default()
+        }),
+    );
+
+    Tool {
+        r#type: chat_completion::ToolType::Function,
+        function: types::Function {
+            name: String::from("read_file"),
+            description: Some(String::from("Read a file")),
+            parameters: types::FunctionParameters {
+                schema_type: types::JSONSchemaType::Object,
+                properties: Some(tool_props),
+                required: Some(vec!["file_path".to_string()]),
+            },
+        }
+    }
+}
+
+pub fn list_all_files_tool() -> Tool {
+    let mut tool_props = HashMap::new();
+    tool_props.insert(
+        "base_path".to_string(),
+        Box::new(types::JSONSchemaDefine {
+            schema_type: Some(types::JSONSchemaType::String),
+            description: Some("The path to get files from".to_string()),
+            ..Default::default()
+        }),
+    );
+
+    Tool {
+        r#type: chat_completion::ToolType::Function,
+        function: types::Function {
+            name: String::from("list_all_files"),
+            description: Some(String::from("List all files in a directory")),
+            parameters: types::FunctionParameters {
+                schema_type: types::JSONSchemaType::Object,
+                properties: Some(tool_props),
+                required: Some(vec!["base_path".to_string()]),
+            },
+        },
+    }
 }
 
 #[cfg(test)]
